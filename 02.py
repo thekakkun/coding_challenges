0cef1d141e5ec51a71e91a1a8c04a1582458b24a
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2021-12-02 12:37:34 -0500

    Day 2 complete

---
+++ 02.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2021/day/2
+
+def navigate_1(commands):
+    h = 0
+    d = 0
+
+    for c in commands:
+        (direction, units) = c.split(' ')
+        units = int(units)
+
+        if direction == 'forward':
+            h += units
+            continue
+        elif direction == 'up':
+            d -= units
+            continue
+        elif direction == 'down':
+            d += units
+            continue
+
+    return h * d
+
+
+def navigate_2(commands):
+    h = 0
+    d = 0
+    a = 0
+
+    for c in commands:
+        (direction, units) = c.split(' ')
+        units = int(units)
+
+        if direction == 'forward':
+            h += units
+            d += a * units
+            continue
+        elif direction == 'up':
+            a -= units
+            continue
+        elif direction == 'down':
+            a += units
+            continue
+
+    return h * d
+
+
+with open('./input/02.txt') as f:
+    commands = f.read().splitlines()
+    print(navigate_1(commands))
+    print(navigate_2(commands))
