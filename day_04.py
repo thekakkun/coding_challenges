7cf40fc35e25485f9308c7a19800bcb6f85410aa
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2021-12-04 13:29:30 -0500

    Day 4 complete

---
+++ day_04.py
@@ -0,0 +1,62 @@
+# https://adventofcode.com/2021/day/4
+
+import math
+
+
+def play_bingo_1(game):
+    (numbers, boards) = game
+
+    for n in numbers:
+        for i, board in enumerate(boards):
+            boards[i] = [-1 if x == n else x for x in board]
+            if is_bingo(boards[i]):
+                return n * sum(x for x in boards[i] if x != -1)
+
+
+def play_bingo_2(game):
+    (numbers, boards) = game
+    remainder = len(boards)
+
+    for n in numbers:
+        for i, board in enumerate(boards):
+            if is_bingo(board):
+                continue
+            else:
+                boards[i] = [-1 if x == n else x for x in board]
+                if is_bingo(boards[i]):
+                    if remainder == 1:
+                        return n * sum(x for x in boards[i] if x != -1)
+                    else:
+                        remainder -= 1
+
+
+def is_bingo(board):
+    board_size = int(math.sqrt(len(board)))
+    bingo_match = [-1] * board_size
+
+    if board.count(-1) < board_size:
+        return False
+    else:
+        return (
+            bingo_match in (board[i::board_size] for i in range(board_size))
+            or
+            bingo_match in (
+                board[i*board_size:(i+1)*board_size] for i in range(board_size))
+        )
+
+
+def parse_bingo(input):
+    numbers, boards = input.strip().split('\n', maxsplit=1)
+
+    numbers = (int(x) for x in numbers.split(','))
+    boards = list(
+        list(int(y) for y in x.replace('\n', ' ').split()) for x in boards.strip().split('\n\n')
+    )
+
+    return (numbers, boards)
+
+
+with open('input/day_04.txt', 'r') as f:
+    input = f.read().strip()
+    print(play_bingo_1(parse_bingo(input)))
+    print(play_bingo_2(parse_bingo(input)))
---
+++ tests/test_day_04.py
@@ -0,0 +1,112 @@
+import day_04
+import unittest
+
+
+class TestDay04(unittest.TestCase):
+    test_input = '''
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+2  0 12  3  7
+'''
+
+    def test_play_bingo_1(self):
+        self.assertEqual(
+            day_04.play_bingo_1(day_04.parse_bingo(self.test_input)),
+            4512
+        )
+
+    def test_play_bingo_2(self):
+        self.assertEqual(
+            day_04.play_bingo_2(day_04.parse_bingo(self.test_input)),
+            1924
+        )
+
+    def test_is_bingo(self):
+        self.assertEqual(
+            day_04.is_bingo(
+                [22, 13, 17, 11, 0,
+                 8, 2, 23, 4, 24,
+                 21, 9, 14, 16, 7,
+                 6, 10, 3, 18, 5,
+                 1, 12, 20, 15, 19]
+            ),
+            False
+        )
+        self.assertEqual(
+            day_04.is_bingo(
+                [22, -1, 17, 11, 0,
+                 8, -1, 23, 4, 24,
+                 21, -1, 14, 16, 7,
+                 6, -1, 3, 18, 5,
+                 1, -1, 20, 15, 19]
+            ),
+            True
+        )
+        self.assertEqual(
+            day_04.is_bingo(
+                [22, 13, 17, 11, 0,
+                 -1, -1, -1, -1, -1,
+                 21, 9, 14, 16, 7,
+                 6, 10, 3, 18, 5,
+                 1, 12, 20, 15, 19]
+            ),
+            True
+        )
+        self.assertEqual(
+            day_04.is_bingo(
+                [-1, 13, -1, 11, 0,
+                 8, 2, -1, 4, 24,
+                 21, 9, 14, 16, 7,
+                 6, -1, 3, 18, 5,
+                 1, 12, 20, -1, 19]
+            ),
+            False
+        )
+
+    def test_parse_bingo(self):
+        self.assertEqual(
+            day_04.parse_bingo(self.test_input)[1],
+            [
+                [
+                    22, 13, 17, 11, 0,
+                    8, 2, 23, 4, 24,
+                    21, 9, 14, 16, 7,
+                    6, 10, 3, 18, 5,
+                    1, 12, 20, 15, 19
+                ],
+                [
+                    3, 15, 0, 2, 22,
+                    9, 18, 13, 17, 5,
+                    19, 8, 7, 25, 23,
+                    20, 11, 10, 24, 4,
+                    14, 21, 16, 12, 6
+                ],
+                [
+                    14, 21, 17, 24, 4,
+                    10, 16, 15, 9, 19,
+                    18, 8, 23, 26, 20,
+                    22, 11, 13, 6, 5,
+                    2,  0, 12, 3, 7
+                ]
+            ]
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
