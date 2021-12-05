8156d5a4db702ae9d3dce7e16f60af4f40f4bd3e
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2021-12-05 15:03:00 -0500

    Day 5 complete

---
+++ day_05.py
@@ -0,0 +1,50 @@
+# https://adventofcode.com/2021/day/5
+
+
+import itertools
+
+
+def find_vents_1(vents):
+    vents = (x for x in vents if x[0][0] == x[1][0] or x[0][1] == x[1][1])
+    vent_map = map_vents(vents)
+
+    return sum(1 for _, v in vent_map.items() if 2 <= v)
+
+
+def find_vents_2(vents):
+    vent_map = map_vents(vents)
+
+    return sum(1 for _, v in vent_map.items() if 2 <= v)
+
+
+def map_vents(vents):
+    vent_map = {}
+
+    for vent in vents:
+        ((x1, y1), (x2, y2)) = vent
+        x_range = range(x1, x2 + 1) if x1 <= x2 else range(x1, x2-1, -1)
+        y_range = range(y1, y2 + 1) if y1 <= y2 else range(y1, y2-1, -1)
+
+        for vent_loc in itertools.zip_longest(
+            x_range, y_range, fillvalue=min(x_range, y_range, key=len)[0]
+        ):
+            vent_map[vent_loc] = vent_map.get(vent_loc, 0) + 1
+
+    return vent_map
+
+
+def parse_input(input):
+    return tuple(
+        tuple(
+            tuple(int(z) for z in y.split(',')
+                  ) for y in x.split(' -> ')
+        ) for x in input.splitlines()
+    )
+
+
+with open('input/day_05.txt', 'r') as f:
+    input = f.read().strip()
+    vents = parse_input(input)
+
+    print(find_vents_1(vents))
+    print(find_vents_2(vents))
---
+++ tests/test_day_05.py
@@ -0,0 +1,76 @@
+import day_05
+import unittest
+
+
+class TestDay05(unittest.TestCase):
+    test_input = """0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+"""
+
+    def test_parse_input(self):
+        self.assertEqual(
+            day_05.parse_input(self.test_input),
+            (
+                ((0, 9), (5, 9)),
+                ((8, 0), (0, 8)),
+                ((9, 4), (3, 4)),
+                ((2, 2), (2, 1)),
+                ((7, 0), (7, 4)),
+                ((6, 4), (2, 0)),
+                ((0, 9), (2, 9)),
+                ((3, 4), (1, 4)),
+                ((0, 0), (8, 8)),
+                ((5, 5), (8, 2))
+            )
+        )
+
+    def test_map_vents(self):
+        self.assertEqual(
+            day_05.map_vents((((0, 9), (5, 9)),)),
+            {
+                (0, 9): 1,
+                (1, 9): 1,
+                (2, 9): 1,
+                (3, 9): 1,
+                (4, 9): 1,
+                (5, 9): 1
+            }
+        )
+
+        self.assertEqual(
+            day_05.map_vents((((6, 4), (2, 0)),)),
+            {
+                (6, 4): 1,
+                (5, 3): 1,
+                (4, 2): 1,
+                (3, 1): 1,
+                (2, 0): 1
+            }
+        )
+
+        self.assertEqual(
+            day_05.map_vents((((9, 7), (7, 9)),)),
+            {
+                (9, 7): 1,
+                (8, 8): 1,
+                (7, 9): 1,
+            }
+        )
+
+    def test_find_vents_1(self):
+        self.assertEqual(
+            day_05.find_vents_1(day_05.parse_input(self.test_input)),
+            5)
+
+    def test_find_vents_2(self):
+        self.assertEqual(
+            day_05.find_vents_2(day_05.parse_input(self.test_input)),
+            12)
