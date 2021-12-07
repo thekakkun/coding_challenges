0775124a51d1d0cb645eaca8038eddbabc1f9692
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2021-12-07 13:23:42 -0500

    Day 7 complete

---
+++ day_07.py
@@ -0,0 +1,38 @@
+# https://adventofcode.com/2021/day/7
+
+
+def dist_1(x, target): return abs(x - target)
+
+
+def dist_2(x, target): return int(
+    abs(x - target) * (abs(x - target) + 1) / 2)
+
+
+def total_fuel(state, target, dist_f):
+    return sum(dist_f(x, target) for x in state)
+
+
+def align_crabs(state, dist_f):
+    target = int(sum(state) / len(state))
+    guess = next_guess = total_fuel(state, target, dist_f)
+
+    if (
+        total_fuel(state, target, dist_f) <
+        total_fuel(state, target+1, dist_f)
+    ):
+        direction = -1
+    else:
+        direction = +1
+
+    while True:
+        guess = next_guess
+        next_guess = total_fuel(state, target + direction, dist_f)
+        target += direction
+        if guess < next_guess:
+            return guess
+
+
+with open('input/day_07.txt', 'r') as f:
+    state = tuple(int(x) for x in f.read().split(','))
+    print(align_crabs(state, dist_1))
+    print(align_crabs(state, dist_2))
---
+++ tests/test_day_07.py
@@ -0,0 +1,47 @@
+import day_07
+
+import unittest
+
+
+class TestDay07(unittest.TestCase):
+
+    test_input = (16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
+
+    def test_total_fuel(self):
+        self.assertEqual(
+            day_07.total_fuel(self.test_input, 2, day_07.dist_1),
+            37
+        )
+        self.assertEqual(
+            day_07.total_fuel(self.test_input, 1, day_07.dist_1),
+            41
+        )
+        self.assertEqual(
+            day_07.total_fuel(self.test_input, 5, day_07.dist_2),
+            168
+        )
+        self.assertEqual(
+            day_07.total_fuel(self.test_input, 2, day_07.dist_2),
+            206
+        )
+
+    def test_align_crabs(self):
+        self.assertEqual(
+            day_07.align_crabs(
+                self.test_input,
+                day_07.dist_1
+            ),
+            37
+        )
+        self.assertEqual(
+            day_07.align_crabs(
+                self.test_input,
+                day_07.dist_2
+            ),
+            168
+        )
+
+
+if __name__ == '__main__':
+
+    unittest.main()
