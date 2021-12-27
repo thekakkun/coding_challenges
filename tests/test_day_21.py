785fb4546e7353146409f8191a486fbfe9dc83ec
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2021-12-27 16:01:39 -0500

    Removed old test

--- tests/test_day_21.py
+++ tests/test_day_21.py
@@ -10,21 +10,6 @@ class TestDay21(unittest.TestCase):
     def test_parse_input(self):
         self.assertEqual(day_21.parse_input(test_input), (3, 7))
 
-    def test_play(self):
-        test_position = day_21.parse_input(test_input)
-        test_state = (0, test_position, (0, 0), 1)
-
-        test_rules_1 = (day_21.iter_die_deterministic(), 1000)
-        test_paths, _ = day_21.play(test_rules_1, test_state)
-        test_paths = list(test_paths)[0]
-        rolls = (len(test_paths) - 1) * 3
-        (_, _, final_score, _) = test_paths[-1]
-        self.assertEqual(rolls * min(final_score), 739785)
-
-        test_rules_2 = (day_21.iter_die_dirac(), 21)
-        results = day_21.play(test_rules_2, test_state)
-        self.assertEqual(results, [444356092776315, 341960390180808])
-
     def test_simulate(self):
         test_start = day_21.parse_input(test_input)
         test_rules = [(day_21.iter_die_deterministic(i), 1000)
