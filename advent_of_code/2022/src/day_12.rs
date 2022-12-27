e76fd9ab4412bc5ee0fa3bd6dab0920f40d91e83
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2022-12-27 11:40:59 -0500

    Day 12 complete

--- advent_of_code/2022/src/day_12.rs
+++ advent_of_code/2022/src/day_12.rs
@@ -1,18 +1,17 @@
 use std::{
     cell::RefCell,
     collections::{HashMap, VecDeque},
-    ops::Deref,
     rc::Rc,
 };
 
-pub fn parse_file(f: &str) -> (Rc<RefCell<HillSquare>>, Rc<RefCell<HillSquare>>) {
+pub fn parse_file(f: &str) -> (RR<Node>, RR<Node>) {
     let mut graph = HashMap::new();
     let mut start = None;
     let mut end = None;
 
     for (row_ix, row) in f.lines().enumerate() {
         for (col_ix, val) in row.chars().enumerate() {
-            let node = HillSquare::new(val, (row_ix, col_ix));
+            let node = Node::new(val, (row_ix, col_ix));
             if val == 'S' {
                 start = Some(Rc::clone(&node));
             } else if val == 'E' {
@@ -23,13 +22,13 @@ pub fn parse_file(f: &str) -> (Rc<RefCell<HillSquare>>, Rc<RefCell<HillSquare>>)
 
             if 0 < row_ix {
                 if let Some(node_to_north) = &graph.get(&(row_ix - 1, col_ix)) {
-                    HillSquare::connect(Rc::clone(&node), Rc::clone(node_to_north));
+                    Node::connect(&node, node_to_north);
                 }
             }
 
             if 0 < col_ix {
                 if let Some(node_to_west) = &graph.get(&(row_ix, col_ix - 1)) {
-                    HillSquare::connect(Rc::clone(&node), Rc::clone(node_to_west));
+                    Node::connect(&node, node_to_west);
                 }
             }
         }
@@ -41,36 +40,50 @@ pub fn parse_file(f: &str) -> (Rc<RefCell<HillSquare>>, Rc<RefCell<HillSquare>>)
     )
 }
 
-pub fn part_1(input: (Rc<RefCell<HillSquare>>, Rc<RefCell<HillSquare>>)) -> usize {
+pub fn part_1(input: (RR<Node>, RR<Node>)) -> usize {
     let (start, end) = input;
-    let path = HillSquare::get_path(start, end);
+
+    let path = Node::get_path(
+        start,
+        |src, dst| dst.borrow().height as i8 - src.borrow().height as i8 <= 1,
+        |node| Rc::ptr_eq(node, &end),
+    );
 
     path.unwrap().len() - 1
 }
 
-// pub fn part_2(mut input: Barrel) -> u8 {
+pub fn part_2(input: (RR<Node>, RR<Node>)) -> usize {
+    let (_, end) = input;
 
-// }
+    let path = Node::get_path(
+        end,
+        |src, dst| src.borrow().height as i8 - dst.borrow().height as i8 <= 1,
+        |node| node.borrow().height == b'a',
+    );
+
+    path.unwrap().len() - 1
+}
 
 type Coord = (usize, usize);
+type RR<T> = Rc<RefCell<T>>;
 
 #[derive(Debug)]
 enum Path {
     Unexplored,
     Start,
-    Parent(Rc<RefCell<HillSquare>>),
+    Parent(RR<Node>),
 }
 
 #[derive(Debug)]
-pub struct HillSquare {
+pub struct Node {
     height: u8,
     coord: Coord,
-    edges: Vec<Rc<RefCell<Self>>>,
+    edges: Vec<RR<Self>>,
     path: Path,
 }
 
-impl HillSquare {
-    fn new(val: char, coord: Coord) -> Rc<RefCell<Self>> {
+impl Node {
+    fn new(val: char, coord: Coord) -> RR<Self> {
         let height = if val == 'S' {
             b'a'
         } else if val == 'E' {
@@ -87,29 +100,23 @@ impl HillSquare {
         }))
     }
 
-    // check accessibility between nodes, and connect eligible edges
-    fn connect(node_1: Rc<RefCell<Self>>, node_2: Rc<RefCell<Self>>) {
-        if node_1.borrow().height.abs_diff(node_2.borrow().height) <= 1 {
-            node_1.borrow_mut().edges.push(Rc::clone(&node_2));
-            node_2.borrow_mut().edges.push(Rc::clone(&node_1));
-        } else if node_1.deref().borrow().height < node_2.deref().borrow().height {
-            node_2.borrow_mut().edges.push(Rc::clone(&node_1));
-        } else if node_2.deref().borrow().height < node_1.deref().borrow().height {
-            node_1.borrow_mut().edges.push(Rc::clone(&node_2));
-        }
+    fn connect(node_1: &RR<Self>, node_2: &RR<Self>) {
+        node_1.borrow_mut().edges.push(Rc::clone(node_2));
+        node_2.borrow_mut().edges.push(Rc::clone(node_1));
     }
 
     fn get_path(
-        start_node: Rc<RefCell<HillSquare>>,
-        end_node: Rc<RefCell<HillSquare>>,
-    ) -> Result<Vec<Rc<RefCell<HillSquare>>>, &'static str> {
+        start_node: RR<Node>,
+        is_accessible: impl Fn(&RR<Node>, &RR<Node>) -> bool, // takes src and dst node, tests for accessibility.
+        is_end: impl Fn(&RR<Node>) -> bool,
+    ) -> Result<Vec<RR<Node>>, &'static str> {
         let mut queue = VecDeque::from([Rc::clone(&start_node)]);
         start_node.borrow_mut().path = Path::Start;
 
         while let Some(node) = queue.pop_front() {
-            if Rc::ptr_eq(&node, &end_node) {
-                let mut path = vec![Rc::clone(&end_node)];
-                let mut node = Rc::clone(&end_node);
+            if is_end(&node) {
+                let mut path = vec![Rc::clone(&node)];
+                let mut node = Rc::clone(&node);
 
                 while let Path::Parent(next_node) = &node.clone().borrow().path {
                     path.push(Rc::clone(next_node));
@@ -120,7 +127,12 @@ impl HillSquare {
                 return Ok(path);
             }
 
-            for neighbor in &node.borrow().edges {
+            for neighbor in node
+                .borrow()
+                .edges
+                .iter()
+                .filter(|&neighbor| is_accessible(&node, neighbor))
+            {
                 let mut mut_neighbor = neighbor.borrow_mut();
 
                 match mut_neighbor.path {
@@ -147,51 +159,9 @@ mod tests {
 
         // check start node
         assert_eq!((0, 0), start.borrow().coord);
-        // connected to southern node
-        assert!(start
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (1, 0)));
-        // connected to eastern node
-        assert!(start
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (0, 1)));
 
         // check end node
         assert_eq!((2, 5), end.borrow().coord);
-        //not connected to northern node
-        assert!(end
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (1, 5)));
-        //not connected to southern node
-        assert!(end
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (3, 5)));
-        //connected to eastern node
-        assert!(end
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (2, 4)));
-        //not connected to western node
-        assert!(end
-            .borrow()
-            .edges
-            .iter()
-            .map(|node| node.borrow().coord)
-            .any(|x| x == (2, 6)));
     }
 
     #[test]
@@ -200,11 +170,11 @@ mod tests {
         assert_eq!(31, part_1(input))
     }
 
-    // #[test]
-    // fn example_part_2() {
-    //     let input = parse_file(EXAMPLE_FILE);
-    //     assert_eq!(2713310158, part_2(input))
-    // }
+    #[test]
+    fn example_part_2() {
+        let input = parse_file(EXAMPLE_FILE);
+        assert_eq!(29, part_2(input))
+    }
 
     const EXAMPLE_FILE: &str = "\
 Sabqponm
--- advent_of_code/2022/src/main.rs
+++ advent_of_code/2022/src/main.rs
@@ -35,14 +35,14 @@ fn main() {
         part_1_split.duration_since(parse_file_split)
     );
 
-    // let input = day_12::parse_file(&f);
-    // let part_1_split = Instant::now();
+    let input = day_12::parse_file(&f);
+    let part_1_split = Instant::now();
 
-    // let part_2_result = day_12::part_2(input);
-    // let done = Instant::now();
-    // println!(
-    //     "Part 2: {} in {:?}",
-    //     part_2_result,
-    //     done.duration_since(part_1_split)
-    // );
+    let part_2_result = day_12::part_2(input);
+    let done = Instant::now();
+    println!(
+        "Part 2: {} in {:?}",
+        part_2_result,
+        done.duration_since(part_1_split)
+    );
 }
