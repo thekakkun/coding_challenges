d1e8727fd5762c518ac103bb4d81f8b8ab963320
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2023-12-17 14:40:45 -0500

    Rewrite to accomodate part 2

--- advent_of_code/2023/src/day_10.py
+++ advent_of_code/2023/src/day_10.py
@@ -1,76 +1,265 @@
+from abc import ABC, abstractmethod
 from collections import defaultdict
-from dataclasses import dataclass, field
+from dataclasses import InitVar, dataclass, field
 from io import TextIOBase
-from typing import Self
+from typing import Self, Literal
 
 from utils import stopwatch
 
 Coord = tuple[int, int]
+Direction = Literal["north", "south", "east", "west"]
+
+
+def look(coord: Coord, direction: Direction) -> Coord:
+    i, j = coord
+    if direction == "north":
+        return (i - 1, j)
+    elif direction == "south":
+        return (i + 1, j)
+    elif direction == "east":
+        return (i, j + 1)
+    elif direction == "west":
+        return (i, j - 1)
+    else:
+        raise Exception(f"Unknown direction: {direction}")
+
+
+@dataclass
+class Pipe(ABC):
+    coord: Coord
+
+    @abstractmethod
+    def travel(self, going: Direction) -> Direction:
+        ...
+
+    @abstractmethod
+    def lhs(self, going: Direction) -> list[Coord]:
+        ...
+
+    @abstractmethod
+    def rhs(self, going: Direction) -> list[Coord]:
+        ...
+
+
+class NSPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "north":
+            return "north"
+        elif going == "south":
+            return "south"
+        else:
+            raise Exception(f"Can't enter from direction: {going}")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return [look(self.coord, "west")]
+        elif going == "south":
+            return [look(self.coord, "east")]
+        else:
+            raise Exception(f"Can't enter from direction: {going}")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return [look(self.coord, "east")]
+        elif going == "south":
+            return [look(self.coord, "west")]
+        else:
+            raise Exception(f"Can't enter from direction: {going}")
+
+
+class EWPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "east":
+            return "east"
+        elif going == "west":
+            return "west"
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "east":
+            return [look(self.coord, "north")]
+        elif going == "west":
+            return [look(self.coord, "south")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "east":
+            return [look(self.coord, "south")]
+        elif going == "west":
+            return [look(self.coord, "north")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+
+class NEPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "south":
+            return "east"
+        elif going == "west":
+            return "north"
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "south":
+            return []
+        elif going == "west":
+            return [look(self.coord, "south"), look(self.coord, "west")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "south":
+            return [look(self.coord, "south"), look(self.coord, "west")]
+        elif going == "west":
+            return []
+        else:
+            raise Exception("Can't enter from that direction.")
+
+
+class NWPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "south":
+            return "west"
+        elif going == "east":
+            return "north"
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "south":
+            return [look(self.coord, "south"), look(self.coord, "east")]
+        elif going == "east":
+            return []
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "south":
+            return []
+        elif going == "east":
+            return [look(self.coord, "south"), look(self.coord, "east")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+
+class SWPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "north":
+            return "west"
+        elif going == "east":
+            return "south"
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return []
+        elif going == "east":
+            return [look(self.coord, "north"), look(self.coord, "east")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return [look(self.coord, "north"), look(self.coord, "east")]
+        elif going == "east":
+            return []
+        else:
+            raise Exception("Can't enter from that direction.")
+
+
+class SEPipe(Pipe):
+    def travel(self, going: Direction) -> Direction:
+        if going == "north":
+            return "east"
+        elif going == "west":
+            return "south"
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def lhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return []
+        elif going == "west":
+            return [look(self.coord, "north"), look(self.coord, "west")]
+        else:
+            raise Exception("Can't enter from that direction.")
+
+    def rhs(self, going: Direction) -> list[Coord]:
+        if going == "north":
+            return [look(self.coord, "north"), look(self.coord, "west")]
+        elif going == "west":
+            return []
+        else:
+            raise Exception("Can't enter from that direction.")
 
 
 @dataclass
 class Field:
-    start: Coord = field(init=False)
-    pipes: dict[Coord, list[Coord]] = field(default_factory=lambda: defaultdict(list))
+    input: InitVar[TextIOBase]
 
-    @classmethod
-    def from_input(cls, input: TextIOBase) -> Self:
-        field = cls()
+    start_coord: Coord = field(init=False)
+    start_dir: Direction = field(init=False)
+    pipes: dict[Coord, Pipe] = field(default_factory=dict)
 
+    def __post_init__(self, input: TextIOBase):
         for i, line in enumerate(input):
             for j, char in enumerate(line.strip()):
                 if char == "S":
-                    field.start = (i, j)
+                    self.start_coord = (i, j)
                 elif char == ".":
                     continue
                 else:
-                    field.add_pipe(char, (i, j))
+                    self.add_pipe(char, (i, j))
 
-        return field
+        # Check to see what direction you can travel to from the start.
+        directions: list[Direction] = ["north", "south", "east", "west"]
+        for direction in directions:
+            try:
+                next_coord = look(self.start_coord, direction)
+                self.pipes[next_coord].travel(direction)
+                self.start_dir = direction
+                break
+            except:
+                continue
 
-    def add_pipe(self, p_type: str, coord: Coord):
-        pipe_type = {
-            "|": lambda i, j: [(i - 1, j), (i + 1, j)],
-            "-": lambda i, j: [(i, j - 1), (i, j + 1)],
-            "L": lambda i, j: [(i - 1, j), (i, j + 1)],
-            "J": lambda i, j: [(i - 1, j), (i, j - 1)],
-            "7": lambda i, j: [(i + 1, j), (i, j - 1)],
-            "F": lambda i, j: [(i + 1, j), (i, j + 1)],
+    def add_pipe(
+        self,
+        pipe_type: str,
+        coord: Coord,
+    ):
+        type_to_pipe = {
+            "|": NSPipe(coord),
+            "-": EWPipe(coord),
+            "L": NEPipe(coord),
+            "J": NWPipe(coord),
+            "7": SWPipe(coord),
+            "F": SEPipe(coord),
         }
 
-        self.pipes[coord] = pipe_type[p_type](*coord)
-
-    def find_loop(self) -> list[Coord]:
-        visited = [self.start]
-
-        # Find the next pipe section from the start pipe
-        (s_i, s_j) = self.start
-        for coord in [(s_i - 1, s_j), (s_i + 1, s_j), (s_i, s_j - 1), (s_i, s_j + 1)]:
-            if self.start in self.pipes.get(coord, []):
-                visited.append(coord)
-                break
-
-        while visited[-1] != self.start:
-            *_, previous, current = visited
-            next_pipe = (  # Could optimize this by checking direction of travel
-                self.pipes[current][0]
-                if self.pipes[current][1] == previous
-                else self.pipes[current][1]
-            )
-            visited.append(next_pipe)
-
-        return visited
+        self.pipes[coord] = type_to_pipe[pipe_type]
 
 
 @stopwatch
 def parse(input: TextIOBase) -> Field:
-    return Field.from_input(input)
+    return Field(input)
 
 
 @stopwatch
 def part_1(field: Field) -> int:
-    loop_path = field.find_loop()
-    return len(loop_path) // 2
+    next_coord = look(field.start_coord, field.start_dir)
+    next_dir = field.start_dir
+    loop_len = 1
+
+    while next_coord != field.start_coord:
+        loop_len += 1
+        next_dir = field.pipes[next_coord].travel(next_dir)
+        next_coord = look(next_coord, next_dir)
+
+    return loop_len // 2
 
 
 if __name__ == "__main__":
--- advent_of_code/2023/tests/test_day_10.py
+++ advent_of_code/2023/tests/test_day_10.py
@@ -2,21 +2,35 @@ from io import StringIO
 
 from src.day_10 import *
 
-input = """7-F7-
+input_1 = """7-F7-
 .FJ|7
 SJLL7
 |F--J
 LJ.LJ
 """
 
+input_2 = """.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"""
+
 
 def test_parse():
-    field = parse(StringIO(input))
-
-    assert field.start == (2, 0)
+    field = parse(StringIO(input_1))
+    assert field.start_coord == (2, 0)
 
 
 def test_part_1():
-    field = parse(StringIO(input))
-
+    field = parse(StringIO(input_1))
     assert part_1(field) == 8
+
+# def test_part_2():
+#     field = parse(StringIO(input_2))
+#     assert part_2(field) == 8
