0ed6534583e20dd1488b1d05169d68d9ea256232
Hiroto Kaku <20251225+thekakkun@users.noreply.github.com>
2024-12-01 16:43:38 -0500

    Day 1 part 1

--- advent_of_code/2024/src/main.rs
+++ advent_of_code/2024/src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+
+pub mod day_01;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input/day_01.txt").unwrap();
+
+    let data = day_01::parse_part_1(&input);
+    let part_1 = day_01::part_1(data);
+    println!("{:?}", part_1)
 }
