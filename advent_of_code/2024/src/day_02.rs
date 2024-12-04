cdf100947d78ef879b5d2722e4f337564368db8a
Hiroto Kaku <hkaku@movableink.com>
2024-12-04 14:38:08 -0500

    Augh!

--- advent_of_code/2024/src/day_02.rs
+++ advent_of_code/2024/src/day_02.rs
@@ -1,3 +1,5 @@
+use std::{iter, ops::RangeInclusive};
+
 type ParsedData = Vec<Vec<i32>>;
 
 pub fn parse_part_1(input: &str) -> ParsedData {
@@ -11,27 +13,20 @@ pub fn parse_part_1(input: &str) -> ParsedData {
         .collect()
 }
 
-fn is_valid(report: &Vec<i32>) -> bool {
-    let mut comparator: Option<fn(&i32, &i32) -> bool> = None;
-
+fn is_valid(report: &Vec<i32>, mut allowed_range: Option<RangeInclusive<i32>>) -> bool {
     let mut report_iter = report.windows(2);
     report_iter.all(|vals: &[i32]| {
-        if let [val_1, val_2] = vals {
-            // Check increase/decrese tolerance
-            if !(1..4 ).contains(&val_1.abs_diff(*val_2)) {
-                return false;
-            }
-
-            // Check direction
-            if let Some(op) = comparator {
-                return op(val_1, val_2);
+        if let [v_1, v_2] = vals {
+            if let Some(range) = &allowed_range {
+                return range.contains(&(v_1 - v_2));
             } else {
-                comparator = match val_1.cmp(val_2) {
-                    std::cmp::Ordering::Less => Some(PartialOrd::lt),
-                    std::cmp::Ordering::Greater => Some(PartialOrd::gt),
-                    std::cmp::Ordering::Equal => return false,
+                allowed_range = match v_1.cmp(v_2) {
+                    std::cmp::Ordering::Less if (-3..=-1).contains(&(v_1 - v_2)) => Some(-3..=-1),
+                    std::cmp::Ordering::Greater if (1..=3).contains(&(v_1 - v_2)) => Some(1..=3),
+                    _ => return false,
                 };
-                return true;
+
+                return allowed_range.as_ref().unwrap().contains(&(v_1 - v_2));
             }
         }
         false
@@ -39,46 +34,175 @@ fn is_valid(report: &Vec<i32>) -> bool {
 }
 
 pub fn solve_part_1(data: ParsedData) -> usize {
-    data.iter().filter(|&report| is_valid(report)).count()
+    data.iter().filter(|&report| is_valid(report, None)).count()
+}
+
+pub fn parse_part_2(input: &str) -> ParsedData {
+    parse_part_1(input)
+}
+
+fn is_valid_with_dampener(report: &Vec<i32>) -> bool {
+    let mut allowed_range: Option<RangeInclusive<_>> = None;
+
+    let mut report_iter = report.iter();
+    let mut val_1 = report_iter.next();
+    let mut val_2 = report_iter.next();
+
+    let mut lookback: Option<&i32> = None;
+
+    loop {
+        if let None = val_2 {
+            return true;
+        }
+
+        if let (Some(&v_1), Some(&v_2)) = (val_1, val_2) {
+            if let Some(ref range) = allowed_range {
+                if range.contains(&(v_1 - v_2)) {
+                } else {
+                    return is_valid(
+                        &iter::once(v_1)
+                            .chain(report_iter.clone().cloned())
+                            .collect(),
+                        allowed_range.clone(),
+                    ) || match lookback {
+                        Some(&lb) => is_valid(
+                            &iter::once(lb)
+                                .chain(iter::once(v_2))
+                                .chain(report_iter.cloned())
+                                .collect(),
+                            allowed_range,
+                        ),
+                        None => is_valid(
+                            &iter::once(v_2).chain(report_iter.cloned()).collect(),
+                            allowed_range,
+                        ),
+                    };
+                }
+            } else {
+                match v_1.cmp(&v_2) {
+                    std::cmp::Ordering::Less if (-3..=-1).contains(&(v_1 - v_2)) => {
+                        allowed_range = Some(-3..=-1);
+                    }
+                    std::cmp::Ordering::Greater if (1..=3).contains(&(v_1 - v_2)) => {
+                        allowed_range = Some(1..=3);
+                    }
+                    _ => {
+                        return {
+                            is_valid(
+                                &iter::once(v_1)
+                                    .chain(report_iter.clone().cloned())
+                                    .collect(),
+                                None,
+                            ) || match lookback {
+                                Some(&lb) => is_valid(
+                                    &iter::once(lb)
+                                        .chain(iter::once(v_2))
+                                        .chain(report_iter.cloned())
+                                        .collect(),
+                                    None,
+                                ),
+                                None => is_valid(
+                                    &iter::once(v_2).chain(report_iter.cloned()).collect(),
+                                    None,
+                                ),
+                            }
+                        }
+                    }
+                }
+            }
+
+            lookback = val_1;
+            val_1 = val_2;
+            val_2 = report_iter.next();
+            continue;
+        }
+    }
+}
+
+pub fn solve_part_2(data: ParsedData) -> usize {
+    data.iter()
+        .filter(|&report| is_valid_with_dampener(report))
+        .count()
 }
 
 #[cfg(test)]
-mod tests {
+mod part_1_tests {
     use crate::day_02::is_valid;
 
     #[test]
     fn decreasing_gradually() {
         let report = vec![7, 6, 4, 2, 1];
-        assert!(is_valid(&report));
+        assert!(is_valid(&report, None));
     }
 
     #[test]
     fn increacing_rapidly() {
         let report = vec![1, 2, 7, 8, 9];
-        assert!(!is_valid(&report));
+        assert!(!is_valid(&report, None));
     }
 
     #[test]
     fn decreasing_rapidly() {
         let report = vec![9, 7, 6, 2, 1];
-        assert!(!is_valid(&report));
+        assert!(!is_valid(&report, None));
     }
 
     #[test]
     fn inconsistent_direction() {
         let report = vec![1, 3, 2, 4, 5];
-        assert!(!is_valid(&report));
+        assert!(!is_valid(&report, None));
     }
 
     #[test]
     fn flat() {
-        let report = vec![8, 6, 4, 4, 15];
-        assert!(!is_valid(&report));
+        let report = vec![8, 6, 4, 4, 1];
+        assert!(!is_valid(&report, None));
     }
 
     #[test]
     fn increasing_gradually() {
         let report = vec![1, 3, 6, 7, 9];
-        assert!(!is_valid(&report));
+        assert!(is_valid(&report, None));
+    }
+}
+
+#[cfg(test)]
+mod part_2_tests {
+    use crate::day_02::is_valid_with_dampener;
+
+    #[test]
+    fn decreasing_gradually() {
+        let report = vec![7, 6, 4, 2, 1];
+        assert!(is_valid_with_dampener(&report));
+    }
+
+    #[test]
+    fn increacing_rapidly() {
+        let report = vec![1, 2, 7, 8, 9];
+        assert!(!is_valid_with_dampener(&report));
+    }
+
+    #[test]
+    fn decreasing_rapidly() {
+        let report = vec![9, 7, 6, 2, 1];
+        assert!(!is_valid_with_dampener(&report));
+    }
+
+    #[test]
+    fn inconsistent_direction() {
+        let report = vec![1, 3, 2, 4, 5];
+        assert!(is_valid_with_dampener(&report));
+    }
+
+    #[test]
+    fn flat() {
+        let report = vec![8, 6, 4, 4, 1];
+        assert!(is_valid_with_dampener(&report));
+    }
+
+    #[test]
+    fn increasing_gradually() {
+        let report = vec![1, 3, 6, 7, 9];
+        assert!(is_valid_with_dampener(&report));
     }
 }
--- advent_of_code/2024/src/main.rs
+++ advent_of_code/2024/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     let result_1 = day_02::solve_part_1(data_1);
     println!("{:?}", result_1);
 
-    // let data_2 = day_01::parse_part_2(&input);
-    // let result_2 = day_01::solve_part_2(data_2);
-    // println!("{:?}", result_2);
+    let data_2 = day_02::parse_part_2(&input);
+    let result_2 = day_02::solve_part_2(data_2);
+    println!("{:?}", result_2);
 }
